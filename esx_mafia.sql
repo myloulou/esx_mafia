897b45a6df2e10592e017194949a09827e3ea8b8
myloulou <38054515+myloulou@users.noreply.github.com>
2019-08-11 15:51:26 +0200

    Add files via upload

--- esx_mafia.sql
+++ esx_mafia.sql
@@ -17,8 +17,8 @@ INSERT INTO `org` (`name`, `label`) VALUES
 -- Déchargement des données de la table `jobs_grades`
 --
 
-INSERT INTO `org_gradeorg` (`org_name`, `gradeorg`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
-('mafia', 0, 'soldato', 'Ptite-Frappe', 1500, '{}', '{}'),
-('mafia', 1, 'capo', 'Capo', 1800, '{}', '{}'),
-('mafia', 2, 'consigliere', 'Consigliere', 2100, '{}', '{}'),
-('mafia', 3, 'boss', 'Parain', 2700, '{}', '{}');
+INSERT INTO `org_gradeorg` (`org_name`, `gradeorg`, `name`, `label`, `skin_male`, `skin_female`) VALUES
+('mafia', 0, 'soldato', 'Ptite-Frappe', '{}', '{}'),
+('mafia', 1, 'capo', 'Capo', '{}', '{}'),
+('mafia', 2, 'consigliere', 'Consigliere', '{}', '{}'),
+('mafia', 3, 'boss', 'Parain', '{}', '{}');
